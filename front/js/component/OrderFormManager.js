c2916442b3d3e9cc6c02fcc3ef2fb02b11a892cf
micka-le-dev <mickael.petithory.pro@gmail.com>
2022-12-07 11:31:36 +0100

    rend réutilisable les teste de validation de l'orderForm

--- front/js/component/OrderFormManager.js
+++ front/js/component/OrderFormManager.js
@@ -17,7 +17,8 @@ export class OrderFormManager{
      */
     constructor(form){
         if( ! form instanceof HTMLFormElement)
-            throw new Error('constrictor of OrderFormManager, HTMLFormElement attendu')
+            throw new Error('constructor of OrderFormManager, HTMLFormElement attendu')
+
         this.#form = form
         form.querySelector('#firstName').addEventListener('input', e => this.#verifyFirstName(e.target.value))
         form.querySelector('#lastName').addEventListener('input', e => this.#verifyLastName(e.target.value))
@@ -25,82 +26,183 @@ export class OrderFormManager{
         form.querySelector('#city').addEventListener('input', e => this.#verifyCity(e.target.value))
         form.querySelector('#email').addEventListener('input', e => this.#verifyEmail(e.target.value))
 
-        form.addEventListener('submit', event => this.submit())
+        form.addEventListener('submit', event => this.submit(event))
     }
 
     submit(){
-        
+
     }
 
+    /**
+     * @param {string} firstName
+     * @returns {boolean | undefined} retourne undefined si firstName est null, undefined ou vide
+     */
+    #firstNameIsOk(firstName){
+        console.log("firstNameIsOk avec ",firstName)
+        if( firstName == undefined || firstName == null || firstName === '')
+            return undefined
+        else if( this.regex.nom.test(firstName) )
+            return true
+        else
+            return false
+    }
     /**
      * @param {string} firstName
      */
     #verifyFirstName(firstName){
         console.log("verifyFirstName avec ",firstName)
         const errorElement = this.#form.querySelector('#firstNameErrorMsg')
-        if( firstName == undefined || firstName === '')
-            errorElement.innerText = "Veillez renseigner vôtre prénom, s'il vous plait."
-        else if( this.regex.nom.test(firstName) )
-            errorElement.innerText = ''
-        else
-            errorElement.innerText = "Ceci n'est pas un prénon valide."
+
+        switch(this.#firstNameIsOk(firstName)){
+            case true:
+                errorElement.innerText = ''
+                break
+            case false:
+                errorElement.innerText = "Ceci n'est pas un prénon valide."
+                break
+            case undefined:
+            default:
+                errorElement.innerText = "Veillez renseigner vôtre prénom, s'il vous plait."
+                break
+        }
     }
 
     /**
      * @param {string} lastName
+     * @returns {boolean | undefined} retourne undefined si lastName est null, undefined ou vide
      */
-     #verifyLastName(lastName){
+    #lastNameIsOk(lastName){
+        console.log("lastNameIsOk avec ",lastName)
+        if( lastName == undefined || lastName == null || lastName === '')
+            return undefined
+        else if( this.regex.nom.test(lastName) )
+            return true
+        else
+            return false
+    }
+    /**
+     * @param {string} lastName
+     */
+    #verifyLastName(lastName){
         console.log("verifyLastName avec ",lastName)
         const errorElement = this.#form.querySelector('#lastNameErrorMsg')
-        if( lastName == undefined || lastName === '')
-            errorElement.innerText = "Veillez renseigner vôtre nom, s'il vous plait."
-        else if( this.regex.nom.test(lastName) )
-            errorElement.innerText = ''
-        else
-            errorElement.innerText = "Ceci n'est pas un nom valide."
+
+        switch(this.#lastNameIsOk(lastName)){
+            case true:
+                errorElement.innerText = ''
+                break
+            case false:
+                errorElement.innerText = "Ceci n'est pas un nom valide."
+                break
+            case undefined:
+            default:
+                errorElement.innerText = "Veillez renseigner vôtre nom, s'il vous plait."
+                break
+        }
     }
 
     /**
      * @param {string} address
+     * @returns {boolean | undefined} retourne undefined si address est null, undefined ou vide
      */
-     #verifyAddress(address){
+     #addressIsOk(address){
+        console.log("addressIsOk avec ",address)
+        if( address == undefined || address == null || address === '')
+            return undefined
+        else if( this.regex.adresse.test(address) )
+            return true
+        else
+            return false
+    }
+    /**
+     * @param {string} address
+     */
+    #verifyAddress(address){
         console.log("verifyAddress avec ",address)
         const errorElement = this.#form.querySelector('#addressErrorMsg')
-        if( address == undefined || address === '')
-            errorElement.innerText = "Veillez renseigner vôtre adresse, s'il vous plait."
-        else if( this.regex.adresse.test(address) )
-            errorElement.innerText = ''
-        else
-            errorElement.innerText = "Ceci n'est pas une adresse valide."
+
+        switch(this.#addressIsOk(address)){
+            case true:
+                errorElement.innerText = ''
+                break
+            case false:
+                errorElement.innerText = "Ceci n'est pas un adresse valide."
+                break
+            case undefined:
+            default:
+                errorElement.innerText = "Veillez renseigner vôtre adresse, s'il vous plait."
+                break
+        }
     }
 
     /**
      * @param {string} city
+     * @returns {boolean | undefined} retourne undefined si city est null, undefined ou vide
      */
-     #verifyCity(city){
+     #cityIsOk(city){
+        console.log("cityIsOk avec ",city)
+        if( city == undefined || city == null || city === '')
+            return undefined
+        else if( this.regex.nom.test(city) )
+            return true
+        else
+            return false
+    }
+    /**
+     * @param {string} city
+     */
+    #verifyCity(city){
         console.log("verifyCity avec ",city)
         const errorElement = this.#form.querySelector('#cityErrorMsg')
-        if( city == undefined || city === '')
-            errorElement.innerText = "Veillez renseigner vôtre ville, s'il vous plait."
-        else if( this.regex.nom.test(city) )
-            errorElement.innerText = ''
-        else
-            errorElement.innerText = "Ceci n'est pas un nom de ville valide."
+
+        switch(this.#cityIsOk(city)){
+            case true:
+                errorElement.innerText = ''
+                break
+            case false:
+                errorElement.innerText = "Ceci n'est pas un ville valide."
+                break
+            case undefined:
+            default:
+                errorElement.innerText = "Veillez renseigner vôtre ville, s'il vous plait."
+                break
+        }
     }
 
 
     /**
      * @param {string} email
+     * @returns {boolean | undefined} retourne undefined si email est null, undefined ou vide
      */
-     #verifyEmail(email){
-        console.log("verifyCity avec ",email)
-        const errorElement = this.#form.querySelector('#emailErrorMsg')
-        if( email == undefined || email === '')
-            errorElement.innerText = "Veillez renseigner vôtre adrersse email, s'il vous plait."
+     #emailIsOk(email){
+        console.log("emailIsOk avec ",email)
+        if( email == undefined || email == null || email === '')
+            return undefined
         else if( this.regex.email.test(email) )
-            errorElement.innerText = ''
+            return true
         else
-            errorElement.innerText = "Ceci n'est pas une adresse email valide."
+            return false
     }
+    /**
+     * @param {string} email
+     */
+    #verifyEmail(email){
+        console.log("emailIsOk avec ",email)
+        const errorElement = this.#form.querySelector('#emailErrorMsg')
+
+        switch(this.#emailIsOk(email)){
+            case true:
+                errorElement.innerText = ''
+                break
+            case false:
+                errorElement.innerText = "Ceci n'est pas un adrersse email valide."
+                break
+            case undefined:
+            default:
+                errorElement.innerText = "Veillez renseigner vôtre adrersse email, s'il vous plait."
+                break
+        }
+    }
+
 
 }
